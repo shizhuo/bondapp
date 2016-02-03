04be85994b0852ea4f6b7328562c9f4eb056e826
szhu <szhu@aiikang.com>
2016-02-03 08:28:57 -0500

    added count_details.sh

---
+++ count_details.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+count=0
+for d1 in details/*
+do
+	for d2 in $d1/*
+	do
+		for d3 in $d2/*
+		do
+			for d4 in $d3/*
+			do
+				count=$((count+1))
+				echo $count
+			done
+		done
+	done
+done
